397719f389c60887ad3534661c7a67a83e5a6b09
Nubis <yo@nubis.im>
2016-04-23 06:28:46 -0300

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,257 @@
+//! Curs: Curl for rust users. Based on Hyper. File Uploads. JSON REST API Clients.
+//!
+//! Make your request, add params and files, curs just works and decides the
+//! right body format and adds any needed headers.
+//!
+//! Then you can optionally deserialize json responses. Or just use them as text.
+//!
+//! [Fork on GitHub](https://github.com/bitex-la/rust-curs)
+//!
+//! #  Examples
+//!
+//! ```
+//! // This example pretty much uses the whole library. Notice hyper is re-exported by curs.
+//! // Uploads a file, and some params doing a POST request to a stubbed server.
+//!
+//! // The actual curs imports.
+//! extern crate curs;
+//! use curs::{Request, FileUpload, DecodableResult, Method};
+//!
+//! // Just stuff needed for this particular test.
+//! extern crate http_stub; 
+//! use http_stub as hs;
+//! use std::env;
+//! use curs::hyper::header::UserAgent;
+//!
+//! fn main(){
+//!  // Nevermind this stub HTTP server. Find the actual curs code below.
+//!  let url = hs::HttpStub::run(|s|{ s.send_body(r#"["foo", "bar"]"#) });
+//!
+//!  let file = FileUpload{
+//!    name: "shim.png".to_string(),
+//!    mime: None,
+//!    path: &env::current_dir().unwrap().join("tests/fixtures/test.png")};
+//!
+//!  let response : Vec<String> = Request::new(Method::Post, &*format!("{}/some_post", url))
+//!    .params(vec![("one","value_one"), ("two", "value_two")])
+//!    .headers(vec![UserAgent("morcilla-firefox".to_string())])
+//!    .files(vec![file])
+//!    .send().decode_success().unwrap();
+//!  assert_eq!(response, vec!["foo", "bar"]);
+//! }
+//! ```
+
+#![feature(custom_derive, plugin)]
+
+pub extern crate serde;
+pub extern crate serde_json;
+pub extern crate hyper;
+extern crate url;
+extern crate rand;
+extern crate mime_guess;
+
+pub use self::hyper::method::Method;
+pub use self::hyper::client::response::Response;
+pub use self::hyper::status::StatusCode;
+
+use std::path::Path;
+use std::fs::File;
+use std::io::Error as IoError;
+use std::io::Read;
+use self::rand::Rng;
+use self::serde::Deserialize;
+use self::hyper::header::{Headers, Header, HeaderFormat, ContentType};
+use self::hyper::client::{Client, IntoUrl};
+use self::hyper::error::Error as HyperError;
+use self::hyper::mime::Mime;
+
+/// Your result may be text or a struct deserialized from JSON.
+/// The error is always a CursError
+pub type CursResult<T> = Result<T,CursError>;
+
+pub trait DecodableResult {
+  fn decode_success<D: Deserialize>(self) -> CursResult<D>;
+}
+
+impl DecodableResult for CursResult<Response> {
+  /// You can chain a decode_success call to your CursResult
+  /// to deserialize a successful (2xx) JSON response. Using serde.
+  fn decode_success<D: Deserialize>(self) -> CursResult<D> {
+    let mut response = try!(self);
+    match response.status {
+      StatusCode::Ok | StatusCode::Created | StatusCode::Accepted => {
+        let mut response_string = String::new();
+        try!(response.read_to_string(&mut response_string));
+        Ok(try!(serde_json::from_str(&response_string)))
+      },
+      _ => Err(CursError::Status(response))
+    }
+  }
+}
+
+/// Sending your request may fail for any of the following reasons.
+#[derive(Debug)]
+pub enum CursError {
+  Status(Response),
+  Network(HyperError),
+  Json(serde_json::Error)
+}
+
+impl From<HyperError> for CursError {
+  fn from(err: HyperError) -> CursError {
+    CursError::Network(err)
+  }
+}
+
+impl From<IoError> for CursError {
+  fn from(i: IoError) -> CursError {
+    CursError::Network(HyperError::Io(i))
+  }
+}
+
+impl From<serde_json::Error> for CursError {
+  fn from(err: serde_json::Error) -> CursError {
+    CursError::Json(err)
+  }
+}
+
+/// All your params should go in a vector.
+pub type Params<'a> = Vec<Param<'a>>;
+
+/// And each param is just a &str tuple.
+pub type Param<'a> = (&'a str, &'a str);
+
+/// File uploads are more than just a path to a local file.
+#[derive(Clone)]
+pub struct FileUpload<'a> {
+  pub name: String,
+  pub mime: Option<Mime>,
+  pub path: &'a Path
+}
+
+/// You're not expected to be using the MultipartBodyBuilder on your own,
+/// Make a Request and it will know when to delegate if there are any files
+/// to be posted. It's still exported publicly because it may come in handy
+/// for other uses.
+pub struct MultipartBodyBuilder {
+  body: Vec<u8>,
+  boundary: String
+}
+
+macro_rules! w {
+  ($b:ident, $f:expr, $a: expr) => (
+    $b.body.extend(format!($f, $a).as_bytes())
+  )
+}
+
+impl MultipartBodyBuilder {
+  pub fn new() -> MultipartBodyBuilder {
+    let mut rng = rand::thread_rng();
+    let boundary: String = rng.gen_ascii_chars().take(30).collect();
+    MultipartBodyBuilder{ body: vec![], boundary: boundary }
+  }
+
+  pub fn build<'a>(mut self, files: Vec<FileUpload>, params: Params<'a>)
+    -> Result<MultipartBodyBuilder, CursError> 
+  {
+    for (name, value) in params {
+      w!(self, "\r\n--{}\r\n", self.boundary);
+      w!(self, "Content-Disposition: form-data; name=\"{}\"", name);
+      w!(self, "\r\n{}\r\n", value);
+    }
+
+    for FileUpload{ name, path, mime } in files {
+      w!(self, "\r\n--{}\r\n", self.boundary);
+      w!(self, "Content-Disposition: form-data; name=\"{}\"", name);
+      w!(self, "; filename=\"{}\"", path.file_name().unwrap().to_str().unwrap());
+      w!(self, "\r\nContent-Type: {}\r\n\r\n",
+        mime.unwrap_or_else(|| self::mime_guess::guess_mime_type(path)));
+
+      let mut contents = try!(File::open(path));
+      try!(contents.read_to_end(&mut self.body));
+      self.body.extend("\r\n\r\n".as_bytes());
+    }
+      
+    w!(self, "\r\n--{}--", self.boundary);
+
+    Ok(self)
+  }
+}
+
+/// The main entry point. Craft your request and send it.
+#[derive(Clone)]
+pub struct Request<'a> {
+  method: Method,
+  url: &'a str,
+  params: Params<'a>,
+  headers: Headers,
+  files: Vec<FileUpload<'a>>
+}
+
+impl<'a> Request<'a>{
+  /// You'll always need a method and the url to start.
+  pub fn new(method: Method, url: &'a str) -> Request<'a> {
+    Request{ method: method, url: url,
+      params: vec![], headers: Headers::new(), files: vec![] }
+  }
+
+  /// Add params. This extends the existing params vector.
+  pub fn params<P>(&mut self, additional: P) -> &mut Request<'a>
+    where P: IntoIterator<Item = Param<'a>>
+  {
+    self.params.extend(additional);
+    self
+  }
+
+  /// Add files to upload. This extends the existing files vector.
+  pub fn files<F>(&mut self, additional: F) -> &mut Request<'a>
+    where F: IntoIterator<Item = FileUpload<'a>>
+  {
+    self.files.extend(additional);
+    self
+  }
+
+  /// Add headers. This replaces all existing headers.
+  pub fn headers<A,H>(&mut self, additional: A) -> &mut Request<'a>
+    where
+      A: IntoIterator<Item = H>,
+      H: Header + HeaderFormat
+  {
+    for h in additional {
+      self.headers.set(h);
+    }
+    self
+  }
+
+  /// Send your request and see what happens.
+  pub fn send(&self) -> CursResult<Response> {
+    let multipart_raw_body : Box<[u8]>; // We define it here for lifetime reasons.
+    let params_as_query = &*url::form_urlencoded::serialize(&self.params);
+    let mut url_string = self.url.into_url().unwrap().serialize();
+    if self.params.len() > 0 && (self.method == Method::Get || self.method == Method::Head)  {
+      url_string = [&*url_string, "?", params_as_query].concat()
+    }
+    let client = Client::new();
+    let mut request = client
+      .request(self.method.clone(), &*url_string)
+      .headers(self.headers.clone());
+
+    if self.method != Method::Get && self.method != Method::Head {
+      request = if self.files.len() == 0 {
+        request
+          .header(ContentType("application/x-www-form-urlencoded".parse().unwrap()))
+          .body(params_as_query)
+      }else{
+        let builder =
+          try!(MultipartBodyBuilder::new()
+            .build(self.files.clone(), self.params.clone()));
+        let raw_mime = ["multipart/form-data; boundary=", &*builder.boundary].concat();
+        multipart_raw_body = builder.body.into_boxed_slice();
+        request
+          .header(ContentType(raw_mime.parse().unwrap()))
+          .body(&*multipart_raw_body)
+      }
+    };
+    Ok(try!(request.send()))
+  }
+}
---
+++ tests/lib.rs
@@ -0,0 +1,96 @@
+#![feature(custom_derive, plugin)]
+#![plugin(serde_macros)]
+
+extern crate curs;
+extern crate http_stub;
+extern crate serde;
+
+use std::env;
+use curs::hyper::header::UserAgent;
+use curs::hyper::method::Method;
+use curs::{Request, DecodableResult, CursResult, CursError, FileUpload};
+use http_stub::HttpStub;
+use http_stub as hs;
+
+#[derive(Deserialize, Debug, PartialEq)]
+struct DummyJson { foo: String }
+
+#[test]
+fn successful_multipart(){
+  let url = HttpStub::run(|mut stub|{
+    stub.got_path("/some_post");
+    stub.got_method(hs::Method::Post);
+    stub.got_header("content-type", "multipart/form-data; boundary");
+    stub.got_header("user-agent", "morcilla-firefox");
+    stub.got_body(r"Content-Type: image/png");
+    stub.got_body(r#"name="shim.png"; filename="test.png""#);
+    stub.got_body(r#"Content-Disposition: form-data; name="two"\r\nvalue_two\r\n"#);
+
+    stub.send_status(hs::StatusCode::Ok);
+    stub.send_header(hs::header::ContentType(
+      hs::Mime(hs::TopLevel::Application, hs::SubLevel::Json, vec![])));
+    stub.send_body(r#"{"foo":"got files"}"#);
+  });
+
+  let file = FileUpload{
+    name: "shim.png".to_string(),
+    mime: None,
+    path: &env::current_dir().unwrap().join("tests/fixtures/test.png")};
+
+  let response : DummyJson = Request::new(Method::Post, &*format!("{}/some_post", url))
+    .params(vec![("one","value_one"), ("two", "value_two")])
+    .headers(vec![UserAgent("morcilla-firefox".to_string())])
+    .files(vec![file])
+    .send().decode_success().unwrap();
+  assert_eq!(response, DummyJson{ foo: "got files".to_string() });
+}
+
+#[test]
+fn successful_json_get(){
+  let url = HttpStub::run(|stub|{
+    stub.got_path(r"/a_get\?one=value_one&two=value_two");
+    stub.got_body("");
+    stub.got_method(hs::Method::Get);
+    stub.got_header("user-agent", "morcilla-firefox");
+    stub.send_body(r#"{"foo":"bar"}"#);
+  });
+
+  let response : DummyJson = Request::new(Method::Get, &*format!("{}/a_get", url))
+    .params(vec![("one","value_one"), ("two", "value_two")])
+    .headers(vec![UserAgent("morcilla-firefox".to_string())])
+    .send().decode_success().unwrap();
+  assert_eq!(response, DummyJson{ foo: "bar".to_string() });
+}
+
+#[test]
+fn successful_json_post(){
+  let url = HttpStub::run(|stub|{
+    stub.got_path("/some_post");
+    stub.got_method(hs::Method::Post);
+    stub.got_body("one=value_one&two=value_two");
+    stub.send_body(r#"{"foo":"that"}"#);
+  });
+
+  let response : DummyJson = Request::new(Method::Post, &*format!("{}/some_post", url))
+    .params(vec![("one","value_one"), ("two", "value_two")])
+    .send().decode_success().unwrap();
+  assert_eq!(response, DummyJson{ foo: "that".to_string() });
+}
+
+#[test]
+fn errors_out_with_not_found(){
+  let url = HttpStub::run(|mut stub|{
+    stub.got_body("");
+    stub.got_method(hs::Method::Get);
+    stub.send_status(hs::StatusCode::InternalServerError);
+    stub.send_body("404 not found");
+  });
+
+  let result : CursResult<DummyJson> =
+    Request::new(Method::Get, &*url).send().decode_success();
+
+  match result.unwrap_err() {
+    CursError::Status(_) => { },
+    _ => panic!("No status error")
+  }
+}
